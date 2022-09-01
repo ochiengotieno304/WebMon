6fe6a14cf5a483361a695b88c2f914166ac7da58
Ochieng Otieno <ochiengotieno71@gmail.com>
2022-09-01 22:06:34 +0300

    add database

--- app.py
+++ app.py
@@ -1,12 +1,51 @@
 import atexit
 import requests
-from flask import Flask, render_template, request
-from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from flask import Flask, render_template, request, redirect
+from flask_apscheduler import APScheduler
+from flask_sqlalchemy import SQLAlchemy
+
+
+class Config:
+    SCHEDULER_API_ENABLED = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///websites.db'
 
 
 app = Flask(__name__)
+app.config.from_object(Config())
 
-websites = []
+# initialize db
+db = SQLAlchemy(app)
+
+
+# creta db
+class Websites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    url = db.Column(db.String(255), nullable=False)
+    interval = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+
+# initialize scheduler
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+
+@scheduler.task('interval', id='do_job_1', seconds=30, misfire_grace_time=900)
+def job1():
+    test("https://google.com")
+
+
+def blah():
+    with scheduler.app.app_context():
+        pass
+
+
+def test(url):
+    contents = requests.get(url)
+    print(contents.status_code)
 
 
 @app.route('/')
@@ -18,25 +57,20 @@ def index():
         website_url = request.form.get('url')
         monitor_interval = request.form.get('interval')
 
-        status_code = test(website_url)
+        new_wwebsite = Websites(title=website_title,
+                                url=website_url,
+                                interval=monitor_interval)
 
-        websites.append(f"{website_title} {website_url} {monitor_interval} {status_code}")
-    
-
-    return render_template('index.html', title=title, websites=websites)
-
-
-def test(url):
-    contents = requests.get(url)
-    return contents.status_code
-
-
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(func=test, trigger="interval", seconds=10)
-# scheduler.start()
-
-# # Shut down the scheduler when exiting the app
-# atexit.register(lambda: scheduler.shutdown())
+        # push to db
+        try:
+            db.session.add(new_wwebsite)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error adding website"
+    else:
+        websites = Websites.query.order_by(Websites.date_created)
+        return render_template('index.html', title=title, websites=websites)
 
 
 if __name__ == '__main__':
